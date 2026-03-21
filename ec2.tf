5be6fb00b7d315700b52c5950dbd22d093e0f056
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-21 20:40:09 +0530

    Moved db ec2 declaration upwards
    because db private ip needed for backend

--- ec2.tf
+++ ec2.tf
@@ -44,6 +44,17 @@ resource "aws_instance" "nat-instance" {
   }
 }
 
+resource "aws_instance" "database-server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.database-subnet.id
+  user_data = join("\n", [local.base-init, local.docker-install])
+
+  tags = {
+    Name = "database-server"
+  }
+}
+
 resource "aws_instance" "backend-server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
@@ -55,13 +66,3 @@ resource "aws_instance" "backend-server" {
   }
 }
 
-resource "aws_instance" "database-server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.database-subnet.id
-  user_data = join("\n", [local.base-init, local.docker-install])
-
-  tags = {
-    Name = "database-server"
-  }
-}
