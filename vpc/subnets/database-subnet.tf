01320b8cb6c64e88489d344557041e549fd3ca20
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-21 17:27:09 +0530

    Declared 4 subnets
    1. Management subnet (for bastion host)
    2. public nat subnet (for NAT gateway / NAT instance)
    3. Backend subnet
    4. Database subnet

---
+++ vpc/subnets/database-subnet.tf
@@ -0,0 +1,16 @@
+module "vpc-stuff" {
+  source = "../"
+}
+
+resource "aws_subnet" "management-subnet" {
+  vpc_id     = module.vpc-stuff.alumni-vpc-id
+  cidr_block = "172.16.3.0/24"
+  tags = {
+    Name = "management-subnet"
+  }
+}
+
+# resource "aws_route_table_association" "management-subnet" {
+#     subnet_id = aws_subnet.management-subnet.id
+#     route_table_id = module.vpc-stuff.public-rt-id
+# }
