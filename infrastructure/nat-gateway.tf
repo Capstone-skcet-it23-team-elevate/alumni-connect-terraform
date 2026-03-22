1e45707a1a83a2836a53e9ec9f23f7bad0104ec3
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-22 18:22:55 +0530

    Put the nat gateway on management subnet

--- infrastructure/nat-gateway.tf
+++ infrastructure/nat-gateway.tf
@@ -4,7 +4,7 @@ resource "aws_eip" "nat-gateway-elastic-ip" {
 
 resource "aws_nat_gateway" "alumni-nat-gw" {
   allocation_id = aws_eip.nat-gateway-elastic-ip.id
-  subnet_id = aws_subnet.empty-subnet.id
+  subnet_id = aws_subnet.management-subnet.id
 
   tags = {
     Name = "alumni-nat-gw"
