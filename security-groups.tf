1d6189ceae8dcee7eff6f196cbf09f0570e29a2e
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-22 16:02:14 +0530

    Removed circular dependency sg id
    alb can access anything on egress now

--- security-groups.tf
+++ security-groups.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "alb-sg" {
     from_port = 8000
     to_port = 8000
     protocol = "tcp"
-    security_groups = [ aws_security_group.backend-sg.id ]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
 
