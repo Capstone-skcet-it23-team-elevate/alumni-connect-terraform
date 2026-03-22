ed5dcf28553bb942083641d892901065348efca7
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-22 19:47:45 +0530

    Removed a useless subnet

--- infrastructure/subnets.tf
+++ infrastructure/subnets.tf
@@ -9,17 +9,6 @@ resource "aws_subnet" "management-subnet" {
   }
 }
 
-resource "aws_subnet" "public-nat-subnet" {
-  vpc_id                  = aws_vpc.alumni-connect-vpc.id
-  cidr_block              = "172.16.0.0/24"
-  availability_zone       = "ap-south-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "public-nat-subnet"
-  }
-}
-
 resource "aws_subnet" "backend-subnet" {
   vpc_id            = aws_vpc.alumni-connect-vpc.id
   cidr_block        = "172.16.2.0/24"
