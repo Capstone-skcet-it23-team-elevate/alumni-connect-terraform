69c6a0934b9a9bbfad2a6707aad9b313d02ea774
Shoban Chiddarth <messageshoban@gmail.com>
2026-03-22 18:19:03 +0530

    Created NAT gateway

---
+++ infrastructure/nat-gateway.tf
@@ -0,0 +1,12 @@
+resource "aws_eip" "nat-gateway-elastic-ip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "alumni-nat-gw" {
+  allocation_id = aws_eip.nat-gateway-elastic-ip.id
+  subnet_id = aws_subnet.empty-subnet.id
+
+  tags = {
+    Name = "alumni-nat-gw"
+  }
+}
